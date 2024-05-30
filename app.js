f22f32b19f5f0cf377f59ed77e9298d171992487
LuisL <dev.eng.luisluna@gmail.com>
2024-05-30 16:10:09 +0000

    version 2.0 Complemento terminado

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// document.getElementById('toggle-button').addEventListener('click', function () {
-//     document.body.classList.toggle('night-mode');
-//     document.getElementById('container').classList.toggle('night-mode');
-//     document.getElementById('header').classList.toggle('night-mode');
-//     document.getElementById('main').classList.toggle('night-mode');
-//     document.getElementById('footer').classList.toggle('night-mode');
-// });
-
-
-
 document.querySelector('.tumbler-wrapper').addEventListener('click', _ => document.body.classList.toggle('night-mode'));
-
