ed300f0166f4e27b1e5f00a26ec6665b73b157a3
LuisL <dev.eng.luisluna@gmail.com>
2024-05-30 15:29:02 +0000

    version 1.0 codigo montado night mode

---
+++ app.js
@@ -0,0 +1,12 @@
+// document.getElementById('toggle-button').addEventListener('click', function () {
+//     document.body.classList.toggle('night-mode');
+//     document.getElementById('container').classList.toggle('night-mode');
+//     document.getElementById('header').classList.toggle('night-mode');
+//     document.getElementById('main').classList.toggle('night-mode');
+//     document.getElementById('footer').classList.toggle('night-mode');
+// });
+
+
+
+document.querySelector('.tumbler-wrapper').addEventListener('click', _ => document.body.classList.toggle('night-mode'));
+
